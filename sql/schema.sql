fd9054b6ff0152373c34412d572d29278dcd01a8
TKShadowBlade <katanablack85@gmail.com>
2021-10-17 16:51:17 -0400

    setting up schema query

---
+++ sql/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+USE employeeDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50),
+    salary DECIMAL(10.3),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(50),
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
