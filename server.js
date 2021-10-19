2a45010fc917607c8306a44363858548ee257d6f
TKShadowBlade <katanablack85@gmail.com>
2021-10-19 03:07:39 -0400

    adding test seeds

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ function startUp() {
             {
             type: 'list',
             name: 'begin',
-            message: 'Which of the following would you like to access first?',
+            message: 'Which of the following would you like to do first?',
             choices: ['View', 'Add', 'Update', 'Quit']
             }
         ]).then((res) => {
@@ -40,11 +40,43 @@ function startUp() {
                 case 'Update':
                     updateEmployee();
                     break;
-                case 'Exit':
+                case 'Quit':
                     console.log ('Completed');
-                    break;
                 default:
                     console.log ('Thank you for your attention')
             }
         });
+}
+
+function view() {
+    inquirer
+        .prompt ([
+            {
+            type: 'list',
+            name: 'view',
+            message: 'Which would you like to view?',
+            choices: ['All employees', 'By department', 'By role']
+            }
+        ]).then((res) => {
+            switch (res.view) {
+                case 'All employees':
+                    viewAllEmployees();
+                    break;
+                case 'By department':
+                    viewByDepartment();
+                    break;
+                case 'By role':
+                    viewByRole();
+                default:
+                    console.log('Thank you');
+            }
+        });
+}
+
+function viewAllEmployees() {
+    connection.query('SELECT e.id AS ID, e.first_name AS First, e.last_name AS Last, e.role_id AS role, r.salary AS Salary, m.last_name AS Manager, d.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role r ON e.role_id = r.title LEFT JOIN department d ON r.department_id - d.id', (err, results) => {
+        if (err) throw err;
+        console.table (results);
+        startUp();
+    });
 }
