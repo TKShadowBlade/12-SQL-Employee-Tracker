bd627033244a727efd6e2ee2af8639279aa82863
TKShadowBlade <katanablack85@gmail.com>
2021-10-19 00:05:08 -0400

    setting up initial inquirer prompt

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Required dependencies
 const inquirer = require ("inquirer");
 const mysql = require ("mysql");
-require ("console.table");
+const consoleTable = require ("console.table");
+const { start } = require("repl");
 
 // mysql connection setup
 const connection = mysql.createConnection ({
@@ -9,10 +10,41 @@ const connection = mysql.createConnection ({
     port: 3306,
     user: 'root',
     password: 'PasswordHere',
-    database: 'employeeDB'
+    database: 'employee_db'
 });
 
 connection.connect((err) => {
     if (err) throw err;
+    console.log ('Connection successful');
 
+    startUp();
 });
+
+function startUp() {
+    inquirer
+        .prompt([
+            {
+            type: 'list',
+            name: 'begin',
+            message: 'Which of the following would you like to access first?',
+            choices: ['View', 'Add', 'Update', 'Quit']
+            }
+        ]).then((res) => {
+            switch(res.begin){
+                case 'View':
+                    view();
+                    break;
+                case 'Add':
+                    add();
+                    break;
+                case 'Update':
+                    updateEmployee();
+                    break;
+                case 'Exit':
+                    console.log ('Completed');
+                    break;
+                default:
+                    console.log ('Thank you for your attention')
+            }
+        });
+}
