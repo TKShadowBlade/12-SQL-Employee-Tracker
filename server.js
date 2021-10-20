71fe8d4180a68c902edb782549ee71015e6931dc
TKShadowBlade <katanablack85@gmail.com>
2021-10-20 01:57:36 -0400

    updated add functions

--- server.js
+++ server.js
@@ -162,3 +162,151 @@ function viewByRole() {
     });
 }
 
+function add() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'add',
+                message: 'Choose which you would like to add:',
+                choices: ['Department', 'Role', 'Employee']
+            }
+        ]).then((res) => {
+            switch(res.add){
+                case 'Department':
+                    addDepartment();
+                    break;
+                case 'Role':
+                    addRole();
+                    break;
+                case 'Employee':
+                    addEmployee();
+                    break;
+                default:
+                    console.log('Thank you for your choice')
+            }
+        })
+}
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                name: 'department',
+                type: 'input',
+                message: 'Type the name of the department you wish to add:'
+            }
+        ]).then((answer) => {
+            connection.query(
+                'INSERT INTO department VALUES (DEFAULT, ?',
+                [answer.department],
+                (err) => {
+                    if(err) throw err;
+                    console.log ('Updated successfully');
+                    startUp();
+                }
+            )
+        })
+}
+
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                name: 'role',
+                type: 'input',
+                message: 'Type in the role you would like to add:'
+            },
+            {
+                name: 'salary',
+                type: 'number',
+                message: 'Input salary here:',
+                validate: (value) => {
+                    if(isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                    }
+            },
+            {
+                name: 'department_id',
+                type: 'number',
+                message: 'Input department ID here',
+                validate: (value) => {
+                    if(isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                    }
+            }
+        ]).then((answer) => {
+            connection.query(
+                'INSERT INTO role SET ?',
+                {
+                    title: answer.role,
+                    salary: answer.salary,
+                    department_id: answer.department_id
+                },
+                (err) => {
+                    if(err) throw err;
+                    console.log ('Updated successfully');
+                    startUp();
+                }
+            )
+        })
+}
+
+function addEmployee() {
+    connection.query ('SELCT * FROM role', (err, res) => {
+        if(err) throw err;
+        inquirer
+        .prompt([
+            {
+                name: 'first',
+                type: 'input',
+                message: 'Input employee first name here:'
+            },
+            {
+                name: 'last',
+                type: 'input',
+                message: 'Input employee last name here:'
+            },
+            {
+                name: 'role',
+                type: 'rawlist',
+                choices: function() {
+                    const choiceArray = [];
+                    for (i = 0; i < res.length; i++) {
+                        choiceArray.push(res[i].title)
+                    }
+                    return choiceArray;
+                },
+                message: 'Choose a title:'
+            },
+            {
+                name: 'manager',
+                type: 'number',
+                validate: (value) => {
+                    if(isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                },
+                message: 'Input manager ID here:',
+                default: '1'
+            }
+        ]).then ((answer) => {
+            connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: answer.firstName,
+                    last_name: answer.lastName,
+                    role_id: answer.role,
+                    manager_id: answer.manager
+                }
+            )
+            console.log ("Succesful Add");
+            startUp()
+        });
+    });
+}
